fe5e31496d9b21e516345b38d7f54ab0c40dc861
manar <manar.hani.hossni@gmail.com>
2019-08-30 01:29:18 +0200

    Back arrow

--- app/src/main/java/com/example/localdb/Activities/SavedDataActivity.java
+++ app/src/main/java/com/example/localdb/Activities/SavedDataActivity.java
@@ -1,13 +1,18 @@
 package com.example.localdb.Activities;
 
 
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import android.view.MenuItem;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toolbar;
+
 
 import com.example.localdb.Classes.DatabaseHelper;
 import com.example.localdb.R;
@@ -19,10 +24,19 @@ public class SavedDataActivity extends AppCompatActivity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Saved Data");
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+
+
         setContentView(R.layout.activity_saved_data);
         DatabaseHelper db = new DatabaseHelper(this);
         ArrayList<HashMap<String, String>> userList = db.GetUsers();
@@ -30,5 +44,11 @@ public class SavedDataActivity extends AppCompatActivity {
         ListAdapter adapter = new SimpleAdapter(SavedDataActivity.this, userList, R.layout.list_row,new String[]{"name","age", "job","gender"}, new int[]{R.id.savedName, R.id.savedAge, R.id.savedJob, R.id.savedGender});
         lv.setAdapter(adapter);
 
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), EnterDataActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
     }
 }
